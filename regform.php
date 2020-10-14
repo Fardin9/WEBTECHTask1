5ee9c877dab9517078e2310bd588cc4fac1a177d
Fardin9 <fardinhoque9@gmail.com>
2020-10-14 20:13:47 +0600

    First commit

---
+++ regform.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+    
+
+<body>
+
+<h2 style="text-align:center;">REGISTRATION</h1><br>
+<body style="background-color:yellow;">
+<form action="loginaction/regsuccessful.php">
+<table style="width:50%">
+  <tr>
+    <td><label for="name">Name:</label></td>
+  <td><input type="text" id="name1" name="name" ></td>
+  </tr>
+ 
+  
+  <tr>
+  <td><label for="email">Email:</label></td>
+  <td><input type="text" id="email" email="email" ></td>
+  
+  </tr>
+  <tr>
+  <td><label for="username">User Name:</label></td>
+  <td><input type="text" id="username" username="username" ></td>
+  
+  </tr>
+  <tr>
+  <td><label for="password">Password:</label></td>
+  <td><input type="text" id="password" password="password" ></td>
+  
+  </tr>
+  <tr>
+  <td><label for="confirmpassword">Confirm Password:</label></td>
+  <td><input type="text" id="cpassword" cpassword="cpassword" ></td>
+  
+  </tr>
+ 
+  <tr>
+  <td><label for="gender">Gender</label></td>
+  </tr>
+  <tr>
+  <td><input type="radio">Male<input type="radio">Female <input type="radio">Other</td>
+  
+  </tr>
+  <tr>
+  <td><label for="dateofbirth">Date of Birth</label></td>
+  </tr>
+  <tr>
+  <td><input type="date" id="date" name="date" min="1900-12-31"></td>
+  </tr>
+  
+  <tr>
+  <td><input type="submit" value="Submit"> <input type="reset" value="Reset"></td>
+  </tr> 
+  </table>
+  </form>
+  
+  
+
+    
+</body>
+</html>
